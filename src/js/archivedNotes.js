aa6481b1c7eb4f4078647a8fe88f2068f633a741
SashaIlchenko <sandra.ilchenko11@gmail.com>
2023-07-28 11:50:20 +0300

    fixed code

--- src/js/archivedNotes.js
+++ src/js/archivedNotes.js
@@ -62,7 +62,7 @@ function onBtnUnarchiveClick(e) {
 }
 
 function onArchive(item) {
-    const archivedItem = item.parentElement.parentElement.getAttribute('data');
+    const archivedItem = item.getAttribute('data');
     const itemIndex = archivedNotes.findIndex(item => item.id === archivedItem);
     if (itemIndex !== -1) {
         unArchiveNote(itemIndex)
